a01dd9fe82a36d65797db0c76f19b9e5dee40504
Noah Rodriguez <nrodrig3@valpo.edu>
2023-06-23 11:56:57 -0500

    UPDATE some of the feed styling

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,12 @@ const page = () => {
   return (
     <section className='w-full flex-center flex-col'>
         <h1 className='head_text text-center'>
-            Discover & Share        
+            Post Your Thoughts
             <br />
-            <span className='orange_gradient text-center'>AI-Powered Prompts</span>
+            <span className='orange_gradient text-center'>User Created Posts</span>
         </h1>
         <p className='desc text-center'>
-            Promptopia is an AI open-source AI prompting tool for modern world to discover, create and share creative prompts
+            Simple post website to practice Next.js, React, and Tailwind. This project was built from JavaScript Mastery's YouTube video. I repurposed and added new features to that starting project.
         </p>
         
         <Feed />
